1a6379a3ef6104ec90fe34caa7f96645ef0f7b01
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-12-30 00:54:30 +0300

    feat: add method plus to base func

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function Base (str) {
   this.str = str;
 }
 
+Base.prototype.plus = function (part) {
+  return this.str + part;
+}
+
 function StringBuilder (str, addStr) {
   Base.call(this, str);
   this.addStr = addStr;
