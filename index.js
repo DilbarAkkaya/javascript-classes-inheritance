f496a4bc4318798b503c61e55c503ad502fe5355
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-12-30 01:47:26 +0300

    feat: change plus method in parent class

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ function Base (str) {
   this.str = str;
 }
 
-Base.prototype.plus = function (part) {
-  return this.str + part;
+Base.prototype.plus = function (...part) {
+  let res = '';
+  for(let i of part) {
+   res += i;
+  }
+  return this.str+res;
 }
 
 function StringBuilder (str, addStr) {
   Base.call(this, str);
-  this.addStr = addStr;
+this.addStr = addStr;
+
 }
 
 StringBuilder.prototype = Object.create(Base.prototype);
 StringBuilder.prototype.constructor = StringBuilder;
 
 const exp = new StringBuilder('aaaa');
-console.log(exp.plus('cccc'))
+console.log(exp.plus('cccc', 'nnnnn'))
