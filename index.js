d585783d23f95db0db8e4437868fd527a2bf5aaf
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-12-31 00:30:52 +0300

    feat: add sub method

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ Base.prototype.multiply = function (){
   throw new Error ('This abstract method will be implement in child');
   
 }
-Base.prototype.remove = function (){
+Base.prototype.divide = function (){
   throw new Error ('This abstract method will be implement in child');
   
 }
@@ -52,5 +52,9 @@ StringBuilder.prototype.divide = function (n) {
   }
    return this;
   }
+  StringBuilder.prototype.sub = function (from, n) {
+      this.arg = this.arg.substring(from, n);
+     return this;
+    }
 const exp = new StringBuilder('dilbar ');
-console.log(exp.plus('aaaa').minus(2).multiply(2).divide(2).remove('dil'));
+console.log(exp.plus('aaaabbbbbbbbccccccc').minus(2).multiply(2).divide(3).remove('dil').sub(6, 1));
