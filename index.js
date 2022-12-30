b95a70ba5af573b339182ecc1ea63490e056d869
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-12-30 10:12:48 +0300

    refactor: change str to arg, for all types arg

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-function Base (str) {
-  this.str = str;
+function Base (arg) {
+  this.arg = arg;
 }
 
 Base.prototype.plus = function (...part) {
-  return this.str + part.reduce(function(item, next){
+  return this.arg + part.reduce(function(item, next){
     return item+next
   })
 }
-function StringBuilder (str, addStr) {
-  Base.call(this, str);
-this.addStr = addStr;
+function StringBuilder (arg, addArg) {
+  Base.call(this, arg);
+this.addArg = addArg;
 
 }
 
 StringBuilder.prototype = Object.create(Base.prototype);
 StringBuilder.prototype.constructor = StringBuilder;
 
-const exp = new StringBuilder('aaaa');
-console.log(exp.plus('cccc', 'nnnnn', 'mmmm'))
+const exp = new StringBuilder(5);
+console.log(exp.plus(3, 5, 3))
