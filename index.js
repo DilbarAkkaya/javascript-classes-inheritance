37d73244dd83a77ca00c550c69b400caeda331c5
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-12-31 00:52:19 +0300

    feat: add get method to base and child, change sub method n+1

--- index.js
+++ index.js
@@ -1,60 +1,70 @@
-function Base (arg) {
+function Base(arg) {
   this.arg = arg;
 }
 
 Base.prototype.plus = function (...part) {
- this.arg = this.arg + part.reduce(function(item, next){
-    return item+next
+  this.arg = this.arg + part.reduce(function (item, next) {
+    return item + next
   })
   return this
 }
-Base.prototype.minus = function (){
-  throw new Error ('This abstract method will be implement in child');
-  
+Base.prototype.minus = function () {
+  throw new Error('This abstract method will be implement in child');
+
 }
 
-Base.prototype.multiply = function (){
-  throw new Error ('This abstract method will be implement in child');
-  
-}
-Base.prototype.divide = function (){
-  throw new Error ('This abstract method will be implement in child');
-  
-}
+Base.prototype.multiply = function () {
+  throw new Error('This abstract method will be implement in child');
 
-function StringBuilder (arg) {
+}
+Base.prototype.divide = function () {
+  throw new Error('This abstract method will be implement in child');
+
+}
+Base.prototype.get = function () {
+  return this.arg;
+}
+function StringBuilder(arg) {
   Base.call(this, arg);
 }
 
 StringBuilder.prototype = Object.create(Base.prototype);
 StringBuilder.prototype.constructor = StringBuilder;
-StringBuilder.prototype.superclass= Base;
+StringBuilder.prototype.superclass = Base;
 
 StringBuilder.prototype.minus = function (char) {
- this.arg = this.arg.slice(0, -char);
- return this;
+  this.arg = this.arg.slice(0, -char);
+  return this;
 }
 
 StringBuilder.prototype.multiply = function (num) {
   var a = [this.arg];
   a.length = num;
   this.arg = a.join(this.arg);
- return this;
+  return this;
 }
 StringBuilder.prototype.divide = function (n) {
- var k = Math.floor((this.arg.length)/n);
- this.arg = this.arg.slice(0,k);
+  var k = Math.floor((this.arg.length) / n);
+  this.arg = this.arg.slice(0, k);
   return this;
- }
- StringBuilder.prototype.remove = function (str) {
-  if(this.arg.match(str)) {
+}
+StringBuilder.prototype.remove = function (str) {
+  if (this.arg.match(str)) {
     this.arg = this.arg.split(`${str}`).join('');
   }
-   return this;
-  }
-  StringBuilder.prototype.sub = function (from, n) {
-      this.arg = this.arg.substring(from, n);
-     return this;
-    }
-const exp = new StringBuilder('dilbar ');
-console.log(exp.plus('aaaabbbbbbbbccccccc').minus(2).multiply(2).divide(3).remove('dil').sub(6, 1));
+  return this;
+}
+StringBuilder.prototype.sub = function (from, n) {
+  this.arg = this.arg.substring(from, n+1);
+  return this;
+}
+const strBuilder = new StringBuilder('Hello');
+console.log(strBuilder
+  .plus(' all', '!')
+  .minus(4)
+  .multiply(3)
+  .divide(4)
+  .remove('l')
+  .sub(1, 1)
+  .get()
+);
