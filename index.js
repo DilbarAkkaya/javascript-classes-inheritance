369c2d6af3098d5532fb15356e1a525592e35a00
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-01-07 00:57:32 +0300

    feat: delete 2 classes

--- index.js
+++ index.js
@@ -62,42 +62,6 @@
       .remove('l')
       .sub(1, 1)
       .get();
-
-  class IntBuilder extends Base {
-    constructor(arg = 0) {
-      super(arg)
-    };
-    static random(from, to) {
-      return Math.floor(Math.random() * (to - from + 1)) + from;
-    };
-    minus(...arg) {
-      this.arg = this.arg - arg.reduce(function (item, next) {
-        return item + next
-      })
-      return this;
-    };
-    multiply(arg) {
-      this.arg = this.arg * arg;
-      return this;
-    };
-    divide(arg) {
-      this.arg = this.arg / arg;
-      return this;
-    };
-    mod(arg) {
-      this.arg = this.arg % arg;
-      return this;
-    };
-  }
-  let intBuilder = new IntBuilder(10);
-    intBuilder
-    .plus(2, 3, 2)
-    .minus(1, 2)
-    .multiply(2)
-    .divide(4)
-    .mod(3)
-    .get();
-  IntBuilder.random(10, 20);
 }
 
 {
@@ -160,47 +124,4 @@
       .mod(3)
       .get();
   IntBuilder.random(10, 100);
-
-  function StringBuilder(arg) {
-    Object.assign(this, new Base(arg || ''));
-  };
-
-  StringBuilder.prototype = Object.create(Base.prototype);
-  StringBuilder.prototype.constructor = StringBuilder;
-  StringBuilder.prototype.superclass = Base;
-  StringBuilder.prototype.minus = function (char) {
-    this.arg = this.arg.slice(0, -char);
-    return this;
-  };
-  StringBuilder.prototype.multiply = function (num) {
-    var a = [this.arg];
-    a.length = num;
-    this.arg = a.join(this.arg);
-    return this;
-  };
-  StringBuilder.prototype.divide = function (n) {
-    var k = Math.floor((this.arg.length) / n);
-    this.arg = this.arg.slice(0, k);
-    return this;
-  };
-  StringBuilder.prototype.remove = function (str) {
-    if (this.arg.match(str)) {
-      this.arg = this.arg.split(`${str}`).join('');
-    }
-    return this;
-  };
-  StringBuilder.prototype.sub = function (from, n) {
-    this.arg = this.arg.substring(from, n + 1);
-    return this;
-  };
-
-  const strBuilder = new StringBuilder('Hello');
-    strBuilder
-      .plus(' all', '!')
-      .minus(4)
-      .multiply(3)
-      .divide(4)
-      .remove('l')
-      .sub(1, 1)
-      .get();
 }
